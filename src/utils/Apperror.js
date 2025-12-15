474528066c527f31f6864c712d3e5fa5b69f6728
sabhi-manu <sabhimanu707@gmail.com>
2025-12-15 09:44:36 +0530

    utils file

---
+++ src/utils/Apperror.js
@@ -0,0 +1,11 @@
+class AppEroor extends Error{
+    constructor( message = "Something went wrong",
+    statusCode = 500,error=null){
+        super(message)
+        this.statusCode = statusCode,
+        this.error = error,
+        this.success = false,
+        this.data = null
+    }
+}
+export {AppEroor}
